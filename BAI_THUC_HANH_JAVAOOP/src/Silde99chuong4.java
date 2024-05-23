beedad9494eee09cb19d26383bc1dfacd0c04774
NguyenHoangSon <sonhoangcr3@gmail.com>
2024-05-23 22:17:50 +0700

    Sidle99 chuong4

---
+++ BAI_THUC_HANH_JAVAOOP/src/Silde99chuong4.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Silde99chuong4 {
+    public static void main(String[] args) {
+        TreeMap<String, Double> treeMap1 = new TreeMap<>();
+        
+        // Khai bao treeMap duoc tao thanh tu Map
+        Map<Float, Integer> map = new HashMap<>();
+        TreeMap<Float, Integer> treeMap2 = new TreeMap<>(map);
+    }
+    
+}
