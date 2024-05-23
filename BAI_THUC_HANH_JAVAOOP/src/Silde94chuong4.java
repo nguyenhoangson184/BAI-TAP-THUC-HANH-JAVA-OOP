367133d336b6f1107d26917c28c7c45522b7518c
NguyenHoangSon <sonhoangcr3@gmail.com>
2024-05-23 22:14:14 +0700

    Silde89chuong4

---
+++ BAI_THUC_HANH_JAVAOOP/src/Silde94chuong4.java
@@ -0,0 +1,22 @@
+import java.util.LinkedHashMap;
+
+public class Silde94chuong4 {
+    public static void main(String[] args) {
+        LinkedHashMap<Integer, String> linkedHashMap = new LinkedHashMap<>();
+
+        linkedHashMap.put(100, "Mo");
+        linkedHashMap.put(101, "Dia");
+        linkedHashMap.put(102, "Chat");
+
+        System.out.println("Before delete : " + linkedHashMap);
+        for(var m : linkedHashMap.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
+        }
+        linkedHashMap.remove(100);
+        System.out.println("After delete : " + linkedHashMap);
+        for(var m : linkedHashMap.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
+        }
+    }
+    
+}
